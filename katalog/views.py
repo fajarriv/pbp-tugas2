69dc74ef900593f9321a8d4ca343e89e9b19525a
fajarriv <rivaldiibnusina@gmail.com>
2022-09-14 15:28:35 +0700

    modify views.py and urls.py

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Fajar Rivaldi Ibnusina',
+    'studentID': '2106707050',
+    }   
+    return render(request, "katalog.html" ,context)
