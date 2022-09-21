951b742378d2690579898eb45a268b571cd9a72a
fajarriv <rivaldiibnusina@gmail.com>
2022-09-21 16:11:25 +0700

    Creating unit test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import *
 
 # Create your tests here.
+class AppTest(TestCase):
+    def test1(self):
+        response_html = Client().get('/mywatchlist/html')
+        response_xml = Client().get('/mywatchlist/xml/')
+        response_json = Client().get('/mywatchlist/json/')
+        self.assertEqual(response_html.status_code,200)
+        self.assertEqual(response_xml.status_code,200)
+        self.assertEqual(response_json.status_code,200)
